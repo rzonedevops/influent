56eae9fdb5f91e23d8555de8a22832087ab50de5
Michael Laccetti <mlaccetti@uncharted.software>
2016-06-13 16:30:40 -0400

    This Java file magically had two licenses!

--- influent-server/src/main/java/influent/server/clustering/utils/ContextRead.java
+++ influent-server/src/main/java/influent/server/clustering/utils/ContextRead.java
@@ -17,33 +17,6 @@
  *  limitations under the License.
  */
 
-/*
- * Copyright (C) 2013-2015 Uncharted Software Inc.
- *
- * Property of Uncharted(TM), formerly Oculus Info Inc.
- * http://uncharted.software/
- *
- * Released under the MIT License.
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy of
- * this software and associated documentation files (the "Software"), to deal in
- * the Software without restriction, including without limitation the rights to
- * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
- * of the Software, and to permit persons to whom the Software is furnished to do
- * so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
-
 package influent.server.clustering.utils;
 
 import influent.idl.FL_Cluster;
@@ -54,7 +27,7 @@ import java.util.Collection;
 import java.util.List;
 
 public interface ContextRead {
-	
+
 	/**
 	 * Returns the context id
 	 */
@@ -74,12 +47,12 @@ public interface ContextRead {
 	 * Return all clusters.
 	 */
 	public List<FL_Cluster> getClusters();
-	
+
 	/**
 	 * Return root level objects in context - either FL_Entity or FL_Cluster objects
 	 */
 	public List<Object> getRootObjects();
-	
+
 	/**
 	 * Return all child context ids
 	 */
@@ -99,14 +72,14 @@ public interface ContextRead {
 	 * Return all entities matching the specified ids.
 	 */
 	public List<FL_Entity> getEntities(Collection<String> ids);
-	
+
 	/**
 	 * Returns whether any entities or clusters exist in context
 	 */
 	public boolean isEmpty();
-	
+
 	/**
 	 * Returns the version of the context; which is updated whenever it is modified
-	 */	
+	 */
 	public int getVersion();
 }
